456a702f084ebf66cf07142a635d7ee3d299fd89
devcanvas <0341531@mail.ru>
2026-02-14 01:21:57 +0300

    Update botst .py

--- botst .py
+++ botst .py
@@ -680,7 +680,7 @@ async def send_affirmation():
     try:
         aff = await get_next_affirmation()
         photo_path = await get_affirmation_photo(aff["image_id"])
-	    caption = f"🌚 {aff['text']}\n\n\n\n\n\nСтавь 🔥 нах\n\n@stervafit"
+        caption = f"🌚 {aff['text']}\n\n\n\n\n\nСтавь 🔥 нах\n\n@stervafit"
         
         await bot.send_photo(
             CHANNEL_ID,
